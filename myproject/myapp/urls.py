54d65131482b70c4553617023c443769ead18978
nicNeutron <69566321+nicNeutron@users.noreply.github.com>
2023-04-25 21:25:34 +0800

    Params and RegEx

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('', views.home),
+    path('home/', views.home),
     path('faq/', views.faq),
     path('date/', views.display_date),
     path('message/', views.message),
+    path('dishes/<str:dish>', views.menuitems), # dish = cheesecake
+    path('menu_item/10', views.menuitems),
+    re_path(r'^menu_item/([0-9]{2})/$', views.menuitems),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -5,7 +5,30 @@ from datetime import datetime
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello world ! This is Home!')
+    path = request.path # access path property inside request object
+    scheme = request.scheme
+    method = request.method
+    address = request.META['REMOTE_ADDR']
+    user_agent = request.META['HTTP_USER_AGENT']
+    path_info = request.path_info
+
+    response = HttpResponse("This Works !")
+    response.headers['Age'] = 20
+
+    msg = f""" <br>
+        <br>Path: {path}
+        <br>Address: {address}
+        <br>Scheme: {scheme}
+        <br>Method: {method}
+        <br>User agent: {user_agent}
+        <br>Path info: {path_info}
+        <br>
+        <br> Response header: {response.headers}
+            
+    """
+
+    #return HttpResponse('Hello world ! This is Home!')
+    return HttpResponse(msg, content_type='text/html', charset='utf-8')
 
 def faq(request):
     return HttpResponse('Hello world ! This is FAQ')
@@ -17,3 +40,13 @@ def display_date(request):
 def message(request):
     text = """<h1 style="color: #F4CE14;"> This is a message in HTML! </h1>"""
     return HttpResponse(text)
+
+def menuitems(request, dish):
+    items = {
+        'pasta': 'Pasta is a type of noodle',
+        'falafel': 'Falafel are deep fried mashed chickpeas',
+        'cheesecake': 'Cheesecake is a creamy dessert'
+    }
+    description = items[dish]
+
+    return HttpResponse(f"<h2> {dish} </h2>" + description)
